f4eb29045359ee351724e436bdc29dcf95a30762
Maurice <maurice.oluoch@8teq.co.ke>
2022-04-25 16:58:35 +0300

    first commit

---
+++ screens/MapScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text style={{ color: 'black', marginTop: 100}}>MapScreen</Text>
+    </View>
+  )
+}
+
+export default MapScreen
+
+const styles = StyleSheet.create({})
